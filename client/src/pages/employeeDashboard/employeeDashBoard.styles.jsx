62c3582d760d870871a60652b36ed00ce0904230
aryanshinde <aryan.shinde20@vit.edu>
2022-10-08 03:52:35 +0530

    Employee UI done

--- client/src/pages/employeeDashboard/employeeDashBoard.styles.jsx
+++ client/src/pages/employeeDashboard/employeeDashBoard.styles.jsx
@@ -1,4 +1,5 @@
 import { styled, Box } from '@mui/material';
+import palette from '../../theme/palette';
 export const PieChartStyles = styled(Box)(() => ({
   width: '24rem',
   height: '15rem',
@@ -20,13 +21,20 @@ export const RightSide = styled(Box)(() => ({
 }));
 export const Card = styled(Box)(() => ({
   width: '18rem',
-  height: '15rem',
+  height: '12rem',
   padding: '15px',
   borderRadius: '30px',
   boxShadow: '10px 10px 20px #cbcdce, -10px -10px 20px #ffffff;',
   display: 'flex',
   flexDirection: 'column',
   margin: '1rem',
+  justifyContent: 'space-between',
+  alignItems: 'flex-start',
+  padding: '1rem',
+  transition: '0.3s ease-in-out',
+  '&:hover': {
+    transform: 'scale(1.04)',
+  },
 }));
 
 export const BarChartStyles = styled(Box)(() => ({
--- client/src/pages/employeeDashboard/employeeDashboard.component.jsx
+++ client/src/pages/employeeDashboard/employeeDashboard.component.jsx
@@ -6,6 +6,9 @@ import {
   Card,
   BarChartStyles,
 } from './employeeDashBoard.styles';
+import { Icon } from '@iconify/react';
+import palette from '../../theme/palette';
+import temp from '../../assets/temp.png';
 const EmployeeDashboard = () => {
   return (
     <Box>
@@ -32,15 +35,31 @@ const EmployeeDashboard = () => {
             sx={{
               width: '12rem',
               height: '12rem',
-              padding: '15px',
               borderRadius: '30px',
               boxShadow: '10px 10px 20px #cbcdce, -10px -10px 20px #ffffff;',
               display: 'flex',
               flexDirection: 'column',
               margin: '1rem',
+              overflow: 'hidden',
+              position: 'relative',
+              padding: '0px',
             }}
           >
-            heheh
+            <img
+              style={{ width: '13rem', height: '13rem' }}
+              src={temp}
+              alt="profile pic"
+            />
+            <Box
+              sx={{
+                position: 'absolute',
+                top: '80%',
+                left: '10%',
+                color: 'white',
+              }}
+            >
+              #248
+            </Box>
           </Box>
           <PieChartStyles></PieChartStyles>
           <PieChartStyles></PieChartStyles>
@@ -55,9 +74,68 @@ const EmployeeDashboard = () => {
               width: '100%',
             }}
           >
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
+            <Card>
+              <Icon
+                icon="clarity:employee-group-solid"
+                width="50px"
+                height="50px"
+                color="black"
+              />
+              <Typography sx={{ fontSize: '1.2rem', fontWeight: 'bold' }}>
+                248 hours
+              </Typography>
+              <Typography
+                sx={{
+                  fontSize: '1.5rem',
+                  fontWeight: 'bold',
+                  color: '#888FA3',
+                }}
+              >
+                Meeting Attended
+              </Typography>
+            </Card>
+
+            <Card>
+              <Icon
+                icon="ic:baseline-work-history"
+                width="50px"
+                height="50px"
+                color="black"
+              />
+              <Typography sx={{ fontSize: '1.2rem', fontWeight: 'bold' }}>
+                248 hours
+              </Typography>
+              <Typography
+                sx={{
+                  fontSize: '1.5rem',
+                  fontWeight: 'bold',
+                  color: '#888FA3',
+                }}
+              >
+                Work Done
+              </Typography>
+            </Card>
+
+            <Card>
+              <Icon
+                icon="ci:coffee-togo"
+                width="50px"
+                height="50px"
+                color="black"
+              />
+              <Typography sx={{ fontSize: '1.2rem', fontWeight: 'bold' }}>
+                248 hours
+              </Typography>
+              <Typography
+                sx={{
+                  fontSize: '1.5rem',
+                  fontWeight: 'bold',
+                  color: '#888FA3',
+                }}
+              >
+                Took Break
+              </Typography>
+            </Card>
           </Box>
 
           <BarChartStyles></BarChartStyles>
