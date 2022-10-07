b576e58c33ed67b1d7df03c61977803092d38baf
aryanshinde <aryan.shinde20@vit.edu>
2022-10-08 01:30:08 +0530

    installed dependencies

--- server/server.js
+++ server/server.js
@@ -1,20 +1,18 @@
-import express from "express";
-import "dotenv/config";
+import express from 'express';
+import 'dotenv/config';
 
-
-import authRoutes from "./routes/auth.routes.js";
-import cors from "cors";
+import authRoutes from './routes/auth.routes.js';
+import cors from 'cors';
 
 const app = express();
 const port = process.env.PORT;
 
-import "./db/conn.js";
+import './db/conn.js';
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/auth", authRoutes);
-
+app.use('/auth', authRoutes);
 
 app.listen(port, () => {
   console.log(`server started on http://localhost:${port}`);
