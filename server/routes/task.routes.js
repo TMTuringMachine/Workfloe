bb3ea05ab8552738056aef67e5c00985bc44c3d7
aryanshinde <aryan.shinde20@vit.edu>
2022-10-09 00:35:46 +0530

    middlewares added

--- server/routes/task.routes.js
+++ server/routes/task.routes.js
@@ -1,9 +1,11 @@
-import express from "express";
+import express from 'express';
 
-import { createTask } from "../controllers/task.controller.js";
+import { createTask } from '../controllers/task.controller.js';
+import activeMiddleware from '../middlewares/isActive.js';
+import authMiddleware from '../middlewares/isAuth.js';
 
 const router = express.Router();
 
-router.post("/createTask/:id", createTask);
+router.post('/createTask/:id', authMiddleware, activeMiddleware, createTask);
 
 export default router;
