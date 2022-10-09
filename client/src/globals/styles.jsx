5e1cf2d65ee474e8c85493d7497458c19bd4be2e
aryanshinde <aryan.shinde20@vit.edu>
2022-10-09 18:26:41 +0530

    logo and fonts added

--- client/src/globals/styles.jsx
+++ client/src/globals/styles.jsx
@@ -1,28 +1,28 @@
-import { Box, Button, styled } from "@mui/material";
-import palette from "../theme/palette";
+import { Box, Button, styled } from '@mui/material';
+import palette from '../theme/palette';
 
 export const CustomButton = styled(Button)(() => ({
-  width: "fit-content",
-  height: "fit-content",
-  padding: "5px 20px",
+  width: 'fit-content',
+  height: 'fit-content',
+  padding: '5px 20px',
   backgroundColor: palette.primary,
   color: palette.surface,
   fontWeight: 600,
-  "&:hover": {
+  '&:hover': {
     backgroundColor: palette.primary,
     color: palette.surface,
   },
 }));
 
 export const MainPage = styled(Box)(({ theme }) => ({
-  width: "100vw",
-  height: "100%",
+  width: '100vw',
+  height: '100%',
   backgroundColor: palette.backgrond1,
-  padding: "30px",
-  [theme.breakpoints.down("lg")]: {
-    padding: "20px",
+  padding: '30px',
+  [theme.breakpoints.down('lg')]: {
+    padding: '20px',
   },
-  [theme.breakpoints.down("md")]: {
-    padding: "15px",
+  [theme.breakpoints.down('md')]: {
+    padding: '15px',
   },
 }));
