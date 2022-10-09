a85b0f55a055885f1a805e1b25e369acba1d3c08
aryanshinde <aryan.shinde20@vit.edu>
2022-10-09 18:39:23 +0530

    fixed typography

--- client/src/components/header/header.styles.jsx
+++ client/src/components/header/header.styles.jsx
@@ -1,57 +1,58 @@
-import { styled, Box } from "@mui/material";
-import palette from "../../theme/palette";
+import { styled, Box } from '@mui/material';
+import palette from '../../theme/palette';
 
 export const HeaderContainer = styled(Box)(() => ({
-  width: "100vw",
-  height: "60px",
+  width: '100vw',
+  height: '60px',
   backgroundColor: palette.primary,
-  display: "flex",
-  flexDirection: "row",
-  alignItems: "center",
-  padding: "5px 30px",
-  justifyContent: "space-between",
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  padding: '5px 30px',
+  justifyContent: 'space-between',
 }));
 
 export const FlexRow = styled(Box)(() => ({
-  display: "flex",
-  flexDirection: "row",
-  alignItems: "center",
-  gap: "30px",
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  gap: '30px',
+  cursor: 'pointer',
 }));
 
-export const CustomSearchBar = styled("input")(({theme}) => ({
-  width: "50vw",
-  outline: "none",
-  padding: "8px 20px",
-  borderRadius: "5px",
-  border: "none",
-  [theme.breakpoints.down("lg")]:{
-    width:"40vw"
+export const CustomSearchBar = styled('input')(({ theme }) => ({
+  width: '50vw',
+  outline: 'none',
+  padding: '8px 20px',
+  borderRadius: '5px',
+  border: 'none',
+  [theme.breakpoints.down('lg')]: {
+    width: '40vw',
   },
-  [theme.breakpoints.down("md")]:{
-    width:"30vw"
-  }
+  [theme.breakpoints.down('md')]: {
+    width: '30vw',
+  },
 }));
 
 export const HeaderMenu = styled(Box)(() => ({
-  width: "200px",
-  padding: "5px",
-  display: "flex",
-  flexDirection: "column",
-  gap: "8px",
+  width: '200px',
+  padding: '5px',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '8px',
 }));
 
 export const HeaderItem = styled(Box)(() => ({
-  width: "100%",
-  height: "fit-content",
-  padding: "8px",
-  cursor: "pointer",
-  display:'flex',
-  gap:'10px',
-  alignItems:'center',
-  "&:hover": {
+  width: '100%',
+  height: 'fit-content',
+  padding: '8px',
+  cursor: 'pointer',
+  display: 'flex',
+  gap: '10px',
+  alignItems: 'center',
+  '&:hover': {
     backgroundColor: palette.primary,
-    borderRadius: "5px",
-    color: "#fff",
+    borderRadius: '5px',
+    color: '#fff',
   },
 }));
