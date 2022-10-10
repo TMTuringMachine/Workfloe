551af66183b55471549aa11cf67ca1689ea763e6
Vedant Gokhale <gokhalevedant06@gmail.com>
2022-10-10 21:15:49 +0530

    Update axiosInstance.js

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -2,7 +2,14 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: 'https://workfloe.vercel.app/',
+  headers: {
+    common: {        // can be common or any other method
+      'Access-Control-Allow-Origin': '*'
+    }
+  }
   // baseURL:"http://localhost:5001/"
 });
+
+// axios.defaults.headers.common['Access-Control-Allow-Origin'] = `*`;
 
 export default axiosInstance;
