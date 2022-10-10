b645c5d16579721672ea9ee4919d4673de5a1730
Vedant Gokhale <gokhalevedant06@gmail.com>
2022-10-10 13:24:40 +0530

    Server on new branch

--- client/src/theme/palette.js
+++
@@ -1,9 +0,0 @@
-const palette = {
-  backgrond1: "#F8FAFB",
-  backgrond2: "#efefef",
-  primary: "#6F6AF8",
-  surface: "#fff",
-  shadow1:"0px 8px 20px rgba(35, 35, 35, 0.1)"
-};
-
-export default palette;
